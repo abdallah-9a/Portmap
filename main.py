2dc1b9acad959d892656357a96fae93b371303ba
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2026-05-30 08:52:53 +0300

    feat: Implement frontend UI

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ import asyncio
 import os
 import signal
 from scraper import get_listening_connections
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+async def read_index():
+    return FileResponse('static/index.html')
 
 @app.websocket("/ws/scan")
 async def websocket_endpoint(websocket: WebSocket):
